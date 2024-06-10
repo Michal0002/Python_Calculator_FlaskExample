84a79ed6898bcd441f01c9001aa8b13e8977eaca
Michał Kasperek <michal.kasperek2000@wp.pl>
2024-06-10 19:46:52 +0200

    template with index.html

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, render_template
 
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 def calculations():
     number1 = float(request.form['number1'])
     number2 = float(request.form['number2'])
@@ -17,4 +22,8 @@ def calculations():
     else:
         result = "Nieprawidłowa operacja"
 
-    
+    return render_template('index.html', result=result)
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)   
